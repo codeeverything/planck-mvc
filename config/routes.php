824ea43d7b40d51d9bbfb7c2278fca5b690ee6af
inkymike <mike@eyeonmogs.com>
2016-03-24 10:12:10 +0000

    Added Junction router. Started implementing

--- config/routes.php
+++ config/routes.php
@@ -1,25 +1,35 @@
 <?php
 
+use Junction\Router;
+$router = new Router();
+
+$router->add('GET /rand/:num?', function () {
+    return [
+        'controller' => 'Rand',
+        'action' => 'rand',
+    ];
+});
+
 /**
  * Defines the routes to be used by your application
  */
 
 // TODO: Pass this responsibility to a Router (Junction for example), and try to keep this decoupled so any router couuld be used
-$routes = array(
-    '/hello' => array(
-        'controller' => 'My',
-        'action' => 'hello',
-    ),
-    '/bye' => array(
-        'controller' => 'My',
-        'action' => 'bye',
-    ),
-    '/rand' => [
-        'controller' => 'Rand',
-        'action' => 'rand',
-    ],
-    '/' => [
-        'controller' => 'My',
-        'action' => 'hello',
-    ],
-);
+// $routes = array(
+//     '/hello' => array(
+//         'controller' => 'My',
+//         'action' => 'hello',
+//     ),
+//     '/bye' => array(
+//         'controller' => 'My',
+//         'action' => 'bye',
+//     ),
+//     '/rand' => [
+//         'controller' => 'Rand',
+//         'action' => 'rand',
+//     ],
+//     '/' => [
+//         'controller' => 'My',
+//         'action' => 'hello',
+//     ],
+// );
