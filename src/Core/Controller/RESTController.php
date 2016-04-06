0434bb62ee75eb197f000fda6de9d6f9df938151
codeeverything <mike@codeeverything.com>
2016-04-06 07:54:04 +0000

    Added some better comments

--- src/Core/Controller/RESTController.php
+++ src/Core/Controller/RESTController.php
@@ -5,6 +5,11 @@ namespace Planck\Core\Controller;
 use Planck\Core\Network\Response;
 use Planck\Core\Network\Request;
 
+/**
+ * A controller to provide some convenience methods for handling RESTful requests and responses
+ * 
+ * @author Mike Timms <mike@codeeverything.com>
+ */
 class RESTController extends Controller {
     
     /**
@@ -38,7 +43,11 @@ class RESTController extends Controller {
     
     /**
      * Set the response to have an appropriate code for objects that have 
-     * been created
+     * been created.
+     * 
+     * In RESTful apps this is typically 201 (CREATED), if the resource is created
+     * immediately, or 202 (ACCEPTED), if the resource is to be created later 
+     * (perhaps pushed onto a message queue or similar)
      * 
      * @param bool $immediate - Whether the object was created immediately, or accepted to be created later
      * @return void
@@ -49,15 +58,41 @@ class RESTController extends Controller {
         $this->response->status($code);
     }
     
+    /**
+     * Set the response to have a code to represent successful retreival requests
+     * where there is some body to the message.
+     * 
+     * IN RESTful apps this is typically 200 (OK)
+     * 
+     * @return void
+     */
     public function ok() {
         $this->response->status(Response::SUCCESS);
     }
     
+    /**
+     * Set the response to have a code to represent successful requests
+     * where there is NO body to the message.
+     * 
+     * IN RESTful apps this is typically 204 (NO CONTENT)
+     * 
+     * @return void
+     */
     public function blank() {
         $this->response->status(Response::NOCONTENT);
         $this->response->body('');
     }
     
+    /**
+     * Set the response to have a code to represent successful requests
+     * where the object being requested is unchanged from the local 
+     * cached version/since the last request for the object.
+     * 
+     * IN RESTful apps this is typically 304 (NOT MODIFIED), and the response
+     * body is expected to be empty
+     * 
+     * @return void
+     */
     public function unchanged() {
         $this->response->status(Response::NOTMODIFIED);
         $this->response->body('');
